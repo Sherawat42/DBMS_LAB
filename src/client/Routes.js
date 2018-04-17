c72bc6b2dd4970e5e17287ca9f606e65fee87214
Manish Sherawat <manishsherawat1@gmail.com>
2018-04-17 14:18:54 +0530

    saving_work

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 // import Home from './components/Home';
 // import UsersList from './components/UsersList';
 // import ImageUpload from './components/ImageUpload';
+import TestComponent from './components/TestComponent';
 
 export default () =>{
-			// <Route exact path="/" component={Home} />
-			// <Route exact path="/users" component={UsersList} />
-			// <Route exact path="/image_upload" component={ImageUpload} />
 	return (
 		<div>
-			<Route exact path="/hi" component={()=>'Hi'} />
+			<Switch>
+				<Route exact path="/hi" component={TestComponent} />
+				<Route exact path="/bye" component={()=>'Bye'} />
+			</Switch>
 		</div>
 	);
 }
